fcf73f320e43653995eb2101d859ac66bafb2918
laymteams <microlaymicks@gmail.com>
2025-12-17 20:45:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN"
 
 # 📦 КОНФИГИ ДЛЯ КАЖДОГО ПОЛЬЗОВАТЕЛЯ
 USER_CONFIGS = {
-    111111111: "vless://config_for_user_1",
-    222222222: "vless://config_for_user_2",
-    333333333: "vless://config_for_user_3",
+    5445296130: "vless://b1a583a0-d9d3-4bdf-915e-6bf9cc061429@144.31.252.107:443?type=grpc&encryption=none&serviceName=&authority=&security=reality&pbk=_cxXv_IKij5XbeAsqEJvRHg-AmjO9A_fT4zeMxrk5CY&fp=chrome&sni=google.com&sid=65&spx=%2F#%D0%9C%D0%BE%D0%B8%20%D0%BF%D0%BE%D0%B4%D0%BE%D0%BF%D0%B5%D1%87%D0%BD%D1%8B%D0%B5%20%D0%B2%D0%BF%D0%BD%D1%89%D0%B8%D0%BA%D0%B8)))-%40laymicus",
+    6557932472: "vless://597aa492-a860-4729-a93f-f700d378d37e@144.31.252.107:443?type=grpc&encryption=none&serviceName=&authority=&security=reality&pbk=_cxXv_IKij5XbeAsqEJvRHg-AmjO9A_fT4zeMxrk5CY&fp=chrome&sni=google.com&sid=65&spx=%2F#%D0%9C%D0%BE%D0%B8%20%D0%BF%D0%BE%D0%B4%D0%BE%D0%BF%D0%B5%D1%87%D0%BD%D1%8B%D0%B5%20%D0%B2%D0%BF%D0%BD%D1%89%D0%B8%D0%BA%D0%B8)))-%40qqcascoqq",
+    8064069403: "vless://ff965144-10e9-4951-9f6a-be44154a120d@144.31.252.107:443?type=grpc&encryption=none&serviceName=&authority=&security=reality&pbk=_cxXv_IKij5XbeAsqEJvRHg-AmjO9A_fT4zeMxrk5CY&fp=chrome&sni=google.com&sid=65&spx=%2F#%D0%9C%D0%BE%D0%B8%20%D0%BF%D0%BE%D0%B4%D0%BE%D0%BF%D0%B5%D1%87%D0%BD%D1%8B%D0%B5%20%D0%B2%D0%BF%D0%BD%D1%89%D0%B8%D0%BA%D0%B8)))-%40Mementoquodmorieris",
 }
 
 bot = Bot(token=BOT_TOKEN)
